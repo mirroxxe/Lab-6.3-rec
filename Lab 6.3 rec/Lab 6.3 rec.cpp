33bd1b77ae988f73a1d3174b505243cd2ccecdd4
Volodya <chuchman002@gmail.com>
2023-11-02 18:17:11 +0200

    Add project files.

---
+++ Lab 6.3 rec/Lab 6.3 rec.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <vector>
+#include <cstdlib> 
+#include <ctime>
+
+
+template <typename T>
+void createArray(std::vector<T>& arr, int size) {
+    arr.clear();
+    for (int i = 0; i < size; i++) {
+        T num = rand() % 100; 
+        arr.push_back(num);
+    }
+}
+
+template <typename T>
+void printArray(const std::vector<T>& arr) {
+    for (const T& num : arr) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+}
+
+template <typename T>
+T recursiveSum(const std::vector<T>& arr, size_t index) {
+    if (index == 0) {
+        return arr[index];
+    }
+    else {
+        return arr[index] + recursiveSum(arr, index - 1);
+    }
+}
+
+int main() {
+    int size = 10;
+    std::vector<int> arr;
+
+    srand(static_cast<unsigned>(time(nullptr)));
+
+    createArray(arr, size);
+    std::cout << "masive:" << std::endl;
+    printArray(arr);
+
+    int sum = recursiveSum(arr, arr.size() - 1); 
+    std::cout << "sum masive elements (recursive): " << sum << std::endl;
+
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,34 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include <vector>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+template <typename T>
+T recursiveSum(const std::vector<T>& arr, size_t index) {
+    if (index == 0) {
+        return arr[index];
+    }
+    else {
+        return arr[index] + recursiveSum(arr, index - 1);
+    }
+}
+
+namespace UnitTest1
+{
+    TEST_CLASS(UnitTest1)
+    {
+    public:
+
+        TEST_METHOD(TestRecursiveSum)
+        {
+            std::vector<int> input = { 1, 2, 3, 4, 5 };
+
+            int expected = 15;
+
+            int result = recursiveSum(input, input.size() - 1);
+
+            Assert::AreEqual(expected, result);
+        }
+    };
+}
